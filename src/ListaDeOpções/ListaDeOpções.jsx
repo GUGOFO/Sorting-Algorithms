6a7658788803295911029b2e5a93ac2ff27825e5
GUGOFO <gugofogomes@gmail.com>
2026-03-12 23:14:26 -0300

    Selecionador

---
+++ src/ListaDeOpções/ListaDeOpções.jsx
@@ -0,0 +1,31 @@
+
+import Styles from "./ListaDeOpções.module.css"
+
+function ListaDeOpções(){
+
+    return(
+        <div id={Styles.divListaDeOpcoes}>
+            <ol id={Styles.listaDeOpcoes}>
+                <ul>BUSCA BINARIA</ul>
+                <ul>INSERTION SORT</ul>
+                <ul>SELECT SORT</ul>
+                <ul>MERGE SORT</ul>
+                <ul>QUICK SORT</ul>
+                <ul>BUSCA BINARIA</ul>
+                <ul>INSERTION SORT</ul>
+                <ul>SELECT SORT</ul>
+                <ul>MERGE SORT</ul>
+                <ul>QUICK SORT</ul>
+                <ul>BUSCA BINARIA</ul>
+                <ul>INSERTION SORT</ul>
+                <ul>SELECT SORT</ul>
+                <ul>MERGE SORT</ul>
+                <ul>QUICK SORT</ul>
+            </ol>
+        </div>
+    )
+
+
+}
+
+export default ListaDeOpções
