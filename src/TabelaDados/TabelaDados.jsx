35fba77e942a66a52535b55176c25e7853a4f1e2
GUGOFO <gugofogomes@gmail.com>
2026-03-13 12:32:02 -0300

    Barra slide colunas

--- src/TabelaDados/TabelaDados.jsx
+++ src/TabelaDados/TabelaDados.jsx
@@ -6,16 +6,33 @@ function TabelaDados(){
     const [colunas, setColunas] = useState([1,2,3,4,5,6,7,8,9,10]);
     const [novoValor, setNovoValor] = useState(1);
 
+    function mudarQuantidadeDeColunas(e) {
+        const numDeColunas = Number(e.target.value);
+        const novoArray = Array.from({ length: numDeColunas }, (_, i) => i + 1);
+        setColunas(novoArray);
+    }
+
     return(
-        <div id={Styles.tabela}>
-            {colunas.map((coluna, id) => { 
-                return (
-                    <div className={Styles.coluna}
-                         key={id}
-                         style={{"--altura" : `${(coluna/Math.max(...colunas)) * 100}%`}}>
-                    </div>
-                );
-            })}
+        <div id={Styles.conjuntoTabela}>
+            <div id={Styles.tabela}>
+                {colunas.map((coluna, id) => { 
+                    return (
+                        <div className={Styles.coluna}
+                            key={id}
+                            style={{"--altura" : `${(coluna/Math.max(...colunas)) * 100}%`}}>
+                        </div>
+                    );
+                })}
+            </div>
+            <div id={Styles.DivInputColunas}>
+                <label htmlFor="inputNumDeColunas" id={Styles.texto}>COLUNAS</label>
+                <input type="range"
+                       id="inputNumDeColunas"
+                       className={Styles.inputNumDeColunas}
+                       min={10} max={200}
+                       value={colunas.length} 
+                       onChange={(e) => mudarQuantidadeDeColunas(e)}/>
+            </div>
         </div>
     )
 
