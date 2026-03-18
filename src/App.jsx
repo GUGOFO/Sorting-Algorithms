b0199079e03ae5b7d4f927e93ea888adbe98c33f
GUGOFO <gugofogomes@gmail.com>
2026-03-18 09:07:29 -0300

    Atualização no titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import React, {useState} from 'react'
 import TabelaDados from './TabelaDados/TabelaDados'
 import ListaDeOpções from './ListaDeOpções/ListaDeOpções'
 import './App.css'
 
 function App() {
 
+  const [algoritmo, setAlgoritmo] = useState("Insertion Sort")
+
   return(
     <div id='paginaCompleta'>
       <ListaDeOpções/>
 
       <div id="pagina">
-        <h1 id='titulo'>NOME DO SORT</h1>
+        <h1 id='titulo' >{algoritmo}</h1>
         <TabelaDados/>
       </div>
 
