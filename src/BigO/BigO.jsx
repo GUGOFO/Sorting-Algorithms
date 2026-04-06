b79b809f4f4fa89b0b42e8d20452767825cda45e
GUGOFO <gugofogomes@gmail.com>
2026-04-06 15:11:20 -0300

    Funcional BigO

--- src/BigO/BigO.jsx
+++ src/BigO/BigO.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, {useRef} from "react";
 import Styles from "./BigO.module.css"
 
 function BigO({algoritmoUsado}){
+
+    let bigO = useRef(["n", "n", "n"]);
+
+    switch (algoritmoUsado){
+        case "Insertion Sort":
+            bigO = ["n²", "n²", "n"];
+            break;
+        case "Select Sort":
+            bigO = ["n²", "n²", "n²"];
+            break;
+        case "Merge Sort":
+            bigO = ["n log n", "n log n", "n log n"];
+            break;
+        case "Thanos Sort":
+            bigO = ["∞", "∞", "n"];
+            break;
+        case "Random Sort":
+            bigO = ["∞", "n * n!", "n"];
+            break;
+        case "Epstein Sort":
+            bigO = ["n²", "n²", "n"];
+            break;
+        default:
+            console.log("fodeu");
+            bigO = ["?", "?", "?"];
+            break;
+    }
 
     return (
         <div id={Styles.bigO}>
             <div className={Styles.conjunto}>
                 <h1 className={Styles.titulo}>Pior Caso</h1>
-                <p className={Styles.resto}>O(n)</p>
+                <p className={Styles.resto}>O({bigO[0]})</p>
             </div>
             <div className={Styles.conjunto}>
                 <h1 className={Styles.titulo}>Caso Médio</h1>
-                <p className={Styles.resto}>O(n)</p>
+                <p className={Styles.resto}>O({bigO[1]})</p>
             </div>
             <div className={Styles.conjunto}>
                 <h1 className={Styles.titulo}>Melhor Caso</h1>
-                <p className={Styles.resto}>O(n)</p>
+                <p className={Styles.resto}>O({bigO[2]})</p>
             </div>
         </div>
     )
